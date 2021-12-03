34a147056d4ddae52b191cae0296c213b1af6c2a
Mike Cavaliere <mcavaliere@echobind.com>
2021-12-03 16:16:56 -0500

    refactor: adds global SWR config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,22 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { PageLayout } from '../components/PageLayout';
+import { SWRConfig } from 'swr';
+
+const swrConfig = {
+  refreshInterval: 0,
+};
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <Provider session={session}>
-      <ChakraProvider>
-        <PageLayout>
-          <Component {...pageProps} />
-        </PageLayout>
-      </ChakraProvider>
+      <SWRConfig value={swrConfig}>
+        <ChakraProvider>
+          <PageLayout>
+            <Component {...pageProps} />
+          </PageLayout>
+        </ChakraProvider>
+      </SWRConfig>
     </Provider>
   );
 }
