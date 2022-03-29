2ce88bca8d31ce61dcda1863088a5711bafab906
Mike Cavaliere <mike@cavaliere.org>
2022-03-29 17:16:38 -0400

    fix: CORS fetch

--- lib/api/timer.ts
+++ lib/api/timer.ts
@@ -1,7 +1,11 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const start = async (): Promise<any> => {
-  return await fetch(`${API_BASE_URL}/rounds/start`, {
+export const start = async (): Promise<any> =>
+  fetch(`${API_BASE_URL}/rounds/start`, {
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+    },
     method: 'POST',
+    mode: 'cors',
   });
-};
