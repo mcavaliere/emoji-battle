0b371be39450bf253c2ce2af9a2cfa6b94160760
Mike Cavaliere <mcavaliere@echobind.com>
2022-06-17 13:50:05 -0400

    fix: types

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -2,12 +2,12 @@ import { Container, Flex } from '@chakra-ui/react';
 
 export const PageLayout = ({ children }) => (
   <Container
-    maxW='100%'
-    direction='column'
-    align='center'
-    justify='center'
-    minH='100vh'
-    width='100%'
+    maxW="100%"
+    flexDirection="column"
+    alignItems="center"
+    justifyContent="center"
+    minH="100vh"
+    width="100%"
   >
     {children}
   </Container>
