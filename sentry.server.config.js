ad85674f32a2e2ca2571f4a3c4e2b3c044b16537
Mike Cavaliere <mcavaliere@echobind.com>
2022-07-19 16:35:40 -0400

    fix: disable sentry in dev to prevent hitting quota

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,12 +6,16 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://0e88743c678341d99ed6a8e0727eef0a@o1305279.ingest.sentry.io/6546459',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn:
+      SENTRY_DSN ||
+      'https://0e88743c678341d99ed6a8e0727eef0a@o1305279.ingest.sentry.io/6546459',
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+  });
+}
