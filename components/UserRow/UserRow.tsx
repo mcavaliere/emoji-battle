d1540070b4e3769b2776bccc2961d19e096263f7
Mike Cavaliere <mcavaliere@echobind.com>
2022-08-04 13:36:51 -0400

    fix: background color of user row

--- components/UserRow/UserRow.tsx
+++ components/UserRow/UserRow.tsx
@@ -21,7 +21,7 @@ export const UserRow = ({ id, image, name }) => {
 
       if (user.id === id) {
         controls.start({
-          backgroundColor: ['#F09D51', '#ffffff'],
+          backgroundColor: ['#F09D51', '#EDF2F7 '],
           transition: {
             duration: 1,
           },
